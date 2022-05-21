eb8091366f0af7118e0ca20ffb5df513bc81ad44
Olivia Du <odu@usc.edu>
2022-05-20 17:10:27 -0700

    custom cursors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,6 @@ function Projects() {
     return (
       <div className="projects">
           <p>I love dipping my toes in all kinds of projects! Check them out in my <a href={PORTFOLIOLINK}>portfolio!</a></p>
-          
       </div>
     );
   }
